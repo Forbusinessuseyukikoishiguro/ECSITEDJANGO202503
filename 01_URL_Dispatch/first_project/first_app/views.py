ffce6cb1d6f122cab49e6da6a29c65335934487d
Forbusinessuseyukikoishiguro <yukinote1026@gmail.com>
2025-03-23 12:13:50 +0900

    03231213

--- 01_URL_Dispatch/first_project/first_app/views.py
+++ 01_URL_Dispatch/first_project/first_app/views.py
@@ -6,5 +6,12 @@ def index(request):
     return HttpResponse('<h1>Good Bye</h1>')
 
 def user_page(request, user_name):
-    print(type(user_name),user_name)
+    prin(type(user_name),user_name)
     return HttpResponse(f'<h1>{user_name}Page</h1>')
+
+def number_page(request,user_name, number):
+    # 受け取ったnumberパラメータの型と値をコンソールに出力（デバッグ用）
+    print(type(number), number)
+    
+    # ブラウザに表示するHTMLレスポンスを返す
+    return HttpResponse(f'<h1>{user_name}page Number: {number}</h1>')
