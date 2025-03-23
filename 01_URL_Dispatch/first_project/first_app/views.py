00e8e879cb1708e95bbe139b28ed39b894f94edc
Forbusinessuseyukikoishiguro <yukinote1026@gmail.com>
2025-03-23 11:51:41 +0900

    03231151

--- 01_URL_Dispatch/first_project/first_app/views.py
+++ 01_URL_Dispatch/first_project/first_app/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse('<h1>Good Bye</h1>')
 
+def user_page(request, user_name):
+    print(type(user_name),user_name)
+    return HttpResponse(f'<h1>{user_name}Page</h1>')
