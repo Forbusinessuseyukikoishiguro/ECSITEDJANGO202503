1d259826af93e2904574a0d6ce126b95cae04999
Forbusinessuseyukikoishiguro <yukinote1026@gmail.com>
2025-03-23 15:37:04 +0900

    03231536

--- 03_Template/Templateproject/TemplateApp/views.py
+++ 03_Template/Templateproject/TemplateApp/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def index( request ):
-    return render( request, 'TemplateApp/index.html', context ={"value":"Hello"} )
+    value = "Good Morning"
+    return render( request, 'TemplateApp/index.html', context ={"value":value} )
+
+
+#home関数を作る
+
+def home(request):
+    my_name = 'Yukiko Ishiguro'
+    favorite_fruit = ['Apple', 'Banana', 'Orange']
+    my_info = {'name': 'Yukiko Ishiguro', 'age': 25}
+    
+    return render(request, 'TemplateApp/home.html', 
+                 context={'my_name': my_name, 'favorite_fruit': favorite_fruit, 'my_info': my_info})
