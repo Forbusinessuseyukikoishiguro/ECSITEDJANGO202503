4ea1719b72f47fd2c03674f700618c34867d4bad
Forbusinessuseyukikoishiguro <yukinote1026@gmail.com>
2025-03-24 11:52:06 +0900

    03241151

--- 04_TemplateExam/TemplateApp/urls.py
+++ 04_TemplateExam/TemplateApp/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from.inport views
+from.import views
 
 app_name = 'app'
 
 urlpatterns = [
-    path('home', views.home, name='index'),
+    path('home', views.home, name='home'),
     path('members', views.members, name='members'),
     path('member/<int:id>', views.member, name='member'),
 ]
