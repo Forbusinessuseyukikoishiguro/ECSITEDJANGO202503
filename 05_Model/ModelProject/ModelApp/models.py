6686759f29bbbbf89a90382947c10ab516def944
Forbusinessuseyukikoishiguro <yukinote1026@gmail.com>
2025-03-25 20:55:55 +0900

    Add changes 03252055

--- 05_Model/ModelProject/ModelApp/models.py
+++ 05_Model/ModelProject/ModelApp/models.py
@@ -1,17 +1,14 @@
 from django.db import models
-
+from datetime import date,datetime
 # Create your models here.
 
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    #age = models.IntegerField()
-    #country = models.CharField(max_length=30)
+    birthday = models.DateField(default=(1900,1,1))
+    email = models.EmailField(db_index=True)
+    salary = models.FloatField(null=True)
+    memo = models.TextField()
+    website = models.URLField(null=True)  
+    create_at = models.DateTimeField(default=datetime.now)
     
-    #def __str__(self):
-        #return self.first_name + ' ' + self.last_name
-
-class Sales(models.Model):
-    fee = models.IntegerField()
-    create_at = models.DateTimeField(null=True)
-    update_at = models.DateTimeField(null=True)
