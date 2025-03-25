2d66a41bf5473ce0dc2d256352433c6baf89be60
Forbusinessuseyukikoishiguro <yukinote1026@gmail.com>
2025-03-25 22:45:56 +0900

    Add changes　140レコード挿入まえ

--- 05_Model/ModelProject/ModelApp/models.py
+++ 05_Model/ModelProject/ModelApp/models.py
@@ -2,13 +2,29 @@ from django.db import models
 from datetime import date,datetime
 # Create your models here.
 
-class Person(models.Model):
+class BaseModel(models.Model):
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        abstract = True
+
+################################################
+
+
+class Person(BaseModel):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    birthday = models.DateField(default=(1900,1,1))
+    birthday = models.DateField(default=date(1900, 1, 1))
     email = models.EmailField(db_index=True)
     salary = models.FloatField(null=True)
     memo = models.TextField()
     website = models.URLField(null=True)  
-    create_at = models.DateTimeField(default=datetime.now)
+    
+    class Meta:
+        db_table = 'person'
+        indexes = [
+            models.Index(fields=['first_name', 'last_name']),
+        ]
+        ordering = ['salary']
     
