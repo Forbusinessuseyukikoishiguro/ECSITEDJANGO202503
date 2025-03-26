f25400437232d7fca40d13deb6da7c891eb5350e
Forbusinessuseyukikoishiguro <yukinote1026@gmail.com>
2025-03-26 16:25:09 +0900

    03261624

--- 05_Model/ModelProject/main.py
+++ 05_Model/ModelProject/main.py
@@ -4,13 +4,15 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ModelProject.settings")
 from django import setup
 setup()
 
-from ModelApp import Person
+from ModelApp.models import Person
 
-p = Persin{
-    first_name = 'Taro', last_name = 'Sato',
-    birthday = '2000-01-01', email = 'aa@gmail.com'
-    salary = 1000, memo = 'memo',web_site = 'http:/google.com',
-}
+p = Person(
+    first_name='Taro', 
+    last_name='Sato',
+    birthday='2000-01-01', 
+    email='aa@gmail.com',
+    salary=1000, 
+    memo='memo', 
+    website='http://google.com',
+)
 p.save()
-
-
