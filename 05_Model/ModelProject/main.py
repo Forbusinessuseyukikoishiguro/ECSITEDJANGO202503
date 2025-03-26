177d35b33de245af219264e7316b27b0b4a29cea
Forbusinessuseyukikoishiguro <yukinote1026@gmail.com>
2025-03-26 17:29:51 +0900

    03261729

--- 05_Model/ModelProject/main.py
+++ 05_Model/ModelProject/main.py
@@ -6,13 +6,34 @@ setup()
 
 from ModelApp.models import Person
 
-p = Person(
-    first_name='Taro', 
-    last_name='Sato',
-    birthday='2000-01-01', 
-    email='aa@gmail.com',
-    salary=1000, 
-    memo='memo', 
-    website='http://google.com',
-)
-p.save()
+# p = Person(
+#     first_name='Taro', 
+#     last_name='Sato',
+#     birthday='2000-01-01', 
+#     email='aa@gmail.com',
+#     salary=1000, 
+#     memo='memo', 
+#     website='http://google.com',
+# )
+# # p.save()
+
+# p = Person(
+#     first_name='Jiro',  
+#     last_name='Sato4',
+#     email='aa@gmail.com',
+#     salary=None, 
+#     memo='memo taro', 
+#     website=None,
+# )
+#p.save()
+
+
+#create
+Person.objects.create(
+    first_name = 'Jiro', last_name ='Ito',
+    email='bb@gmail.com',
+    salary=20000,
+    memo='creatememethod実行',
+    website=None
+)    
+    
